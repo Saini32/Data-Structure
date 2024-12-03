46c8966464817871b3ebe97cadb7eed926be8551
Sneha Mourya <157492674+Saini32@users.noreply.github.com>
2024-12-03 16:42:57 +0530

    traverse

---
+++ traversing_of_array.cpp
@@ -0,0 +1,16 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+int i,arr[10];
+clrscr();
+cout<<"enter array elements  =";
+for(i=0;i<5;i++)
+{
+cin>>arr[i];
+}
+cout<<"array ele =\n";
+for(i=0;i<5;i++)
+{cout<<arr[i];
+}getch();
+}
